fa4c7819cbf5dca0bbbfd39aee2e3164e0bed83b
Nauzet Hernández <nauzet.aduen@gmail.com>
2019-01-15 23:29:44 +0000

    Add simple container to test our navbar

---
+++ lib/screens/debt_list.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class DebtList extends StatefulWidget {
+  @override
+  _DebtListState createState() => _DebtListState();
+}
+
+class _DebtListState extends State<DebtList> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.green,
+
+    );
+  }
+}
