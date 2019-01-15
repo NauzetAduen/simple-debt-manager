b6e0c261c6c78be1c76e09b3ef391a65ea572b2f
Nauzet Hernández <nauzet.aduen@gmail.com>
2019-01-15 23:31:08 +0000

    Add simple container

---
+++ lib/screens/general_info_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class GeneralInfo extends StatefulWidget {
+  @override
+  _GeneralInfoState createState() => _GeneralInfoState();
+}
+
+class _GeneralInfoState extends State<GeneralInfo> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.yellow,
+    );
+  }
+}
