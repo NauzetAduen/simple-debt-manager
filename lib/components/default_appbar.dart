57efcb2124c1f0a1fd14191ca06c2334d1a6a32b
Nauzet Hernández <nauzet.aduen@gmail.com>
2019-01-15 23:27:38 +0000

    Add a default appbar

---
+++ lib/components/default_appbar.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class DefaultAppBar extends AppBar{
+  @override
+  final Widget title = Text("Default appbar niggawat");
+
+  @override
+  final bool centerTitle = true;
+
+}
