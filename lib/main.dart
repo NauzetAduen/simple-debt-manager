a86fb8158ca53ba5affe20be8da9b8e84fc96514
Nauzet Hernández <nauzet.aduen@gmail.com>
2019-01-15 18:22:23 +0000

    Init

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(App());
+
+class App extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+    );
+  }
+}
