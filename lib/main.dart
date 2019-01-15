c9b61f41d5d157b8d903ac262093a329987fdbcf
Nauzet Hernández <nauzet.aduen@gmail.com>
2019-01-15 23:24:37 +0000

    Change main to load our MainScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
+import 'screens/main_screen.dart';
 
 void main() => runApp(App());
 
 class App extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: "Simple Debt Manager",
+      home: MainScreen(),
     );
   }
 }
