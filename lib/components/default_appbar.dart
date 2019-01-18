a2878f266f6f901ad1fa8c39ae204e953a70522d
Nauzet Hernández <nauzet.aduen@gmail.com>
2019-01-18 01:58:33 +0000

    Remove default appbar

--- lib/components/default_appbar.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-class DefaultAppBar extends AppBar{
-  @override
-  final Widget title = Text("Simple Debt Manager");
-
-  @override
-  final bool centerTitle = true;
-
-}
