ae3d7f4aa13ad9ca9106f433b2ff3e462dd855e4
Nauzet Hernández <nauzet.aduen@gmail.com>
2019-01-18 02:02:05 +0000

    Add global key to show snackbars

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:simple_debt_manager/components/custom_appbar.dart';
 import 'package:simple_debt_manager/screens/debt_list.dart';
 import 'package:simple_debt_manager/screens/general_info_screen.dart';
-import 'package:simple_debt_manager/components/default_appbar.dart';
+import 'package:simple_debt_manager/screens/new_debt.dart';
 
 class MainScreen extends StatefulWidget {
   @override
@@ -10,10 +11,13 @@ class MainScreen extends StatefulWidget {
 
 class _MainScreenState extends State<MainScreen> {
   int _currentIndex = 0;
+    final GlobalKey<ScaffoldState> mainKey = new GlobalKey<ScaffoldState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: DefaultAppBar(),
+      key: mainKey,
+      appBar: CustomAppBar(Text("Simple Debt Manager")),
       body: _getTab(_currentIndex),
       floatingActionButton: _getFAB(),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
@@ -41,8 +45,7 @@ class _MainScreenState extends State<MainScreen> {
       },
       items: [
         BottomNavigationBarItem(icon: Icon(Icons.list), title: Text("List")),
-        BottomNavigationBarItem(
-            icon: Icon(Icons.folder), title: Text("General"))
+        BottomNavigationBarItem(icon: Icon(Icons.folder), title: Text("General"))
       ],
     );
   }
@@ -50,7 +53,9 @@ class _MainScreenState extends State<MainScreen> {
   _getFAB() {
     return FloatingActionButton(
       child: Icon(Icons.add), 
-      onPressed: () {},
+      onPressed: () {
+        Navigator.push(context, MaterialPageRoute(builder: (context) => NewDebt(scaffoldKey: mainKey,)));
+      },
     );
   }
 }
