01fa3f385d74d2f9483297018981605b3cefec9a
Nauzet Hernández <nauzet.aduen@gmail.com>
2019-01-21 03:54:56 +0000

    Add update/delete functionality

--- lib/screens/debt_detail.dart
+++ lib/screens/debt_detail.dart
@@ -3,6 +3,7 @@ import 'package:simple_debt_manager/components/custom_appbar.dart';
 import 'package:simple_debt_manager/models/debt.dart';
 import 'package:simple_debt_manager/styles/styles.dart';
 import 'package:simple_debt_manager/utils/database_helper.dart';
+import 'package:simple_debt_manager/utils/debt_action_result.dart';
 import 'package:simple_debt_manager/utils/validator.dart';
 
 class DebtDetail extends StatefulWidget {
@@ -21,8 +22,18 @@ class _DebtDetailState extends State<DebtDetail> {
   final TextEditingController descController = TextEditingController();
   final TextEditingController quantityController = TextEditingController();
   final TextEditingController paidController = TextEditingController();
+
+ 
+  bool first = true;
+
   @override
   Widget build(BuildContext context) {
+    if (first) {
+      _fillValues();
+      first = false;
+    }
+    double percent = double.parse(paidController.text) /
+        double.parse(quantityController.text);
     return Scaffold(
         appBar: CustomAppBar(Text("${widget.debt.name}")),
         body: Form(
@@ -30,16 +41,12 @@ class _DebtDetailState extends State<DebtDetail> {
             child: Padding(
                 padding: Styles.paddingForm,
                 child: ListView(children: <Widget>[
-                  buildProgressIndicator(0.75),
-                  buildTextField(nameController, "Name", "${widget.debt.name}"),
-                  buildTextField(
-                      debitorController, "Debitor", "${widget.debt.debitor}"),
-                  buildTextField(descController, "Description",
-                      "${widget.debt.description}"),
-                  buildQuantityField(quantityController, "Total Quantity",
-                      "${widget.debt.totalQuantity}"),
-                  buildQuantityField(paidController, "Paid Quantity",
-                      "${widget.debt.paidQuantity}"),
+                  buildProgressIndicator(percent),
+                  buildTextField(nameController, "Name"),
+                  buildTextField(debitorController, "Debitor"),
+                  buildTextField(descController, "Description"),
+                  buildQuantityField(),
+                  buildPaidQuantityField(),
                   buildButtons()
                 ]))));
   }
@@ -57,32 +64,47 @@ class _DebtDetailState extends State<DebtDetail> {
     );
   }
 
-  Padding buildTextField(
-      TextEditingController editController, String field, String value) {
-    editController.text = value;
+  Padding buildTextField(TextEditingController editController, String field) {
     return Padding(
-      padding: Styles.paddingFields,
-      child: TextFormField(
-          validator: Validator.validateString,
+        padding: Styles.paddingFields,
+        child: TextFormField(
           controller: editController,
+          validator: Validator.validateString,
           decoration: InputDecoration(
             labelText: field,
+            border: Styles.borderRounded,
+          ),
+          onFieldSubmitted: (value) {
+            editController.text = value;
+          },
+        ));
+  }
+
+
+  buildQuantityField() {
+    return Padding(
+      padding: Styles.paddingFields,
+      child: TextField(
+          onChanged: (value) => _updateTotalQuantity(),
+          keyboardType: TextInputType.number,
+          //validator: Validator.validateQuantity,
+          controller: quantityController,
+          decoration: InputDecoration(
+            labelText: "Total Quantity",
             border: Styles.borderRounded,
           )),
     );
   }
 
-  buildQuantityField(
-      TextEditingController editController, String field, String value) {
-    editController.text = value;
+  buildPaidQuantityField() {
     return Padding(
       padding: Styles.paddingFields,
       child: TextFormField(
           keyboardType: TextInputType.number,
-          validator: Validator.validateQuantity,
-          controller: editController,
+          validator: _paidValidator,
+          controller: paidController,
           decoration: InputDecoration(
-            labelText: field,
+            labelText: "Paid Quantity",
             border: Styles.borderRounded,
           )),
     );
@@ -116,14 +138,44 @@ class _DebtDetailState extends State<DebtDetail> {
   }
 
   void _update() {
-    debugPrint("UPDATIN");
+    if (_formKey.currentState.validate()) {
+      final Debt updatedDebt = Debt(
+          nameController.text,
+          debitorController.text,
+          descController.text,
+          double.parse(quantityController.text),
+          double.parse(paidController.text));
+      DebtActionResult result = DebtActionResult(
+          action: 1, debtId: widget.debt.id, updateDebt: updatedDebt);
+      dbHelper.updateDebt(updatedDebt);
+      Navigator.pop(context, result);
+    }
   }
-  void _delete(){
+
+  void _delete() {
+    DebtActionResult result =
+        DebtActionResult(action: 2, debtId: widget.debt.id, updateDebt: widget.debt);
     debugPrint("Deletin");
     dbHelper.deleteDebt(widget.debt.id);
-    Navigator.pop(context, true);
+    Navigator.pop(context, result);
+  }
+
+  void _fillValues() {
+    nameController.text = widget.debt.name;
+    debitorController.text = widget.debt.debitor;
+    descController.text = widget.debt.description;
+    quantityController.text = widget.debt.totalQuantity.toString();
+    paidController.text = widget.debt.paidQuantity.toString();
   }
 
 
- 
+  void _updateTotalQuantity() {
+    widget.debt.totalQuantity = double.parse(quantityController.text);
+  }
+
+  String _paidValidator(String value) {
+    if(value.isEmpty)return "This field can not be empty";
+    if(double.parse(paidController.text) > double.parse(quantityController.text)) return "This should be smaller than ${quantityController.text}";
+    return null;
+  }
 }
