4e0d5099e7fe191b0196782c516a7e24919a906d
Nauzet Hernández <nauzet.aduen@gmail.com>
2019-01-24 09:28:54 +0000

    Add CustomPieChart class to draw our piechart

---
+++ lib/components/custom_pie_chart.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:charts_flutter/flutter.dart' as charts;
+
+class CustomPieChart extends StatelessWidget {
+  final List<charts.Series> seriesList;
+  final bool animate;
+
+  CustomPieChart(this.seriesList, {this.animate});
+
+  @override
+  Widget build(BuildContext context) {
+    return charts.PieChart(seriesList,
+      animate: animate,
+      defaultRenderer: new charts.ArcRendererConfig(arcRendererDecorators: [
+          new charts.ArcLabelDecorator(
+              labelPosition: charts.ArcLabelPosition.outside)
+        ]));
+  }
+
+
+}
