6e081f41f4e9ad3b68474da24d5e1e9535140e46
Nauzet Hernández <nauzet.aduen@gmail.com>
2019-01-24 15:42:28 +0000

    Add new list for diferent pie

--- lib/models/graph_data_holder.dart
+++ lib/models/graph_data_holder.dart
@@ -1,5 +1,7 @@
 import 'package:charts_flutter/flutter.dart' as charts;
+import 'package:flutter/material.dart';
 import 'package:simple_debt_manager/models/linear_amount.dart';
+import 'package:simple_debt_manager/models/linear_type.dart';
 
 class GraphDataHolder {
   double _totalAmount;
@@ -8,7 +10,8 @@ class GraphDataHolder {
   int _totalDebtPartialyPaid;
   int _totalDebtZeroPaid;
   double _percent;
-  List<LinearAmount> _seriesList;
+  List<LinearAmount> _totalAmountSeriesList;
+  List<LinearType> _totalTypesSeriesList;
 
   GraphDataHolder(){
     _totalAmount = 0.0;
@@ -17,7 +20,8 @@ class GraphDataHolder {
     _totalDebtPartialyPaid = 0;
     _totalDebtZeroPaid = 0;
     _percent = 0.000;
-    _seriesList = List<LinearAmount>();
+    _totalAmountSeriesList = List<LinearAmount>();
+    _totalTypesSeriesList = List<LinearType>();
   }
 
   double get totalAmount => _totalAmount;
@@ -26,7 +30,8 @@ class GraphDataHolder {
   int get totalDebtPatialyPaid => _totalDebtPartialyPaid;
   int get totalDebtZeroPaid => _totalDebtZeroPaid;
   double get percent => _percent;
-  List<LinearAmount> get seriesList => _seriesList;
+  List<LinearAmount> get totalAmountSeriesList => _totalAmountSeriesList;
+  List<LinearType> get totalTypesSeriesList => _totalTypesSeriesList;
 
   set percent(double value) => this._percent = value;
 
@@ -35,19 +40,41 @@ class GraphDataHolder {
   void incrementTotalFullyPaid() => _totalDebtFullyPaid++;
   void incrementTotalPartialyPaid() => _totalDebtPartialyPaid++;
   void incrementTotalZeroyPaid() => _totalDebtZeroPaid++;
-  void addSerie(LinearAmount serie) => _seriesList.add(serie);
-  List<charts.Series<LinearAmount, String>> toSeries() {
-
+  void addSerieTotalAmount(LinearAmount serie) => _totalAmountSeriesList.add(serie);
+  void addSerieTotalTypes(LinearType serie) => _totalTypesSeriesList.add(serie);
+  List<charts.Series<LinearAmount, String>> totalAmountSeries() {
     return [
       new charts.Series<LinearAmount, String>(
         id: 'TotalAmount',
         domainFn: (LinearAmount amount, _) => amount.type,
         measureFn: (LinearAmount amount, _) => amount.amount,
-        data: _seriesList,
+        colorFn: (LinearAmount amount, _) => getColor(amount.color),
+        data: _totalAmountSeriesList,
         // Set a label accessor to control the text of the arc label.
         labelAccessorFn: (LinearAmount row, _) => '${row.type}',
       )
     ];
+  }
+
+  List<charts.Series<LinearType, String>> totalTypesSeries() {
+    return [
+      new charts.Series<LinearType, String>(
+        id: 'TotalAmount',
+        domainFn: (LinearType amount, _) => amount.type,
+        measureFn: (LinearType amount, _) => amount.amount,
+        colorFn: (LinearType amount, _) => getColor(amount.color),
+        data: _totalTypesSeriesList,
+        // Set a label accessor to control the text of the arc label.
+        labelAccessorFn: (LinearType row, _) => '${row.type}',
+      )
+    ];
+  }
+
+  charts.Color getColor(Color color){
+    return charts.Color(r:color.red, 
+                        b: color.blue,
+                        g: color.green, 
+                        a:color.alpha);
   }
 }
 
