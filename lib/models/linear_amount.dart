cf619d7ffd5a2bcbefa6490e17a37178014f7670
Nauzet Hernández <nauzet.aduen@gmail.com>
2019-01-24 15:43:17 +0000

    Add new linear class to hold new pie series

--- lib/models/linear_amount.dart
+++ lib/models/linear_amount.dart
@@ -1,6 +1,9 @@
+import 'package:flutter/material.dart';
+
 class LinearAmount{
   final String type;
   final double amount;
+  final Color color;
 
-  LinearAmount(this.type, this.amount);
+  LinearAmount(this.type, this.amount, this.color);
 }
---
+++ lib/models/linear_type.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class LinearType{
+  final String type;
+  final int amount;
+    final Color color;
+
+
+  LinearType(this.type, this.amount, this.color);
+
+}
